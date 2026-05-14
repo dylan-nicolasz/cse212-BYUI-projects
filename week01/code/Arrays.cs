69fc61ec4b2e46a7f8d44d6831781b738e32172c
dylan-nicolasz <dylan.nicolas.zurita@gmail.com>
2026-05-13 23:53:55 -0500

    w01 code: Dynam. Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,21 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
+        //We already have a list to store the data, call the list and give a it name
+        double[] multiples = new double[length]; 
+        for (int i = 0; i < length; i++)  //for loop to iterate over the list, will start at 0 until the length of the list.
+        {
+            multiples [i] = number * (i+1); // It will take the number and se if it's a multiple of, if it is it wil store in the 'multiples' list and continue with the next number
+        }
+
+        
+
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,6 +34,21 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        // we need to tell the program to find the start position from the list will rotate
+        // we take the whole list and substract the amount the user will provide and the result of that will be the index for the list to rotate.
+        int rotateIndex = data.Count - amount;
+        List<int> rotateList = new List<int> ();
+
+
+        //For loop to find the starting index and to keep going until the end of the list.
+        for (int i = rotateIndex; rotateIndex < data.Count; i++)
+        {
+            rotateList.Add(data[i]); //this will take the values and sotore in the new List, and will put the last values first.
+        }
+
+        data.Clear(); // clear the original list
+
+        data.AddRange (rotateList); // copy all the values that were rotated already into the original list.
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
