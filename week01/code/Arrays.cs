75855576a8d779a08f615d30a719a529de48e3f3
dylan-nicolasz <dylan.nicolas.zurita@gmail.com>
2026-05-18 15:27:28 -0500

    learning activites

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,13 +9,13 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         //We already have a list to store the data, call the list and give a it name
-        double[] multiples = new double[length]; 
+        double[] multiples = new double[length];
         for (int i = 0; i < length; i++)  //for loop to iterate over the list, will start at 0 until the length of the list.
         {
-            multiples [i] = number * (i+1); // It will take the number and se if it's a multiple of, if it is it wil store in the 'multiples' list and continue with the next number
+            multiples[i] = number * (i + 1); // It will take the number and se if it's a multiple of, if it is it wil store in the 'multiples' list and continue with the next number
         }
 
-        
+
 
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
@@ -37,7 +37,7 @@ public static class Arrays
         // we need to tell the program to find the start position from the list will rotate
         // we take the whole list and substract the amount the user will provide and the result of that will be the index for the list to rotate.
         int rotateIndex = data.Count - amount;
-        List<int> rotateList = new List<int> ();
+        List<int> rotateList = new List<int>();
 
 
         //For loop to find the starting index and to keep going until the end of the list.
@@ -48,7 +48,7 @@ public static class Arrays
 
         data.Clear(); // clear the original list
 
-        data.AddRange (rotateList); // copy all the values that were rotated already into the original list.
+        data.AddRange(rotateList); // copy all the values that were rotated already into the original list.
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
