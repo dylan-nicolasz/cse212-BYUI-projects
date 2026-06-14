d5cb8c80513937a92ceccc29b50db23e497c07c1
dylan-nicolasz <dylan.nicolas.zurita@gmail.com>
2026-06-13 22:05:16 -0500

    code week5 trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using NuGet.Frameworks;
+
 public class Node
 {
     public int Data { get; set; }
@@ -15,15 +17,14 @@ public class Node
 
         if (value < Data)
         {
-            // Insert to the left
+        
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -34,12 +35,49 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+
+        if (value == Data)
+        {
+            return true;
+        }
+        if (value < Data)
+        {
+            if (Left == null)
+            {
+                return false;
+            }
+            return Left.Contains(value);
+        }
+
+        if (Right == null)
+        {
+            return false;
+        }
+        return Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left != null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+        if (Right != null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+
+        if (leftHeight > rightHeight)
+        {
+            return leftHeight + 1;
+        }
+        else
+        {
+            return rightHeight + 1;
+        }
     }
 }
